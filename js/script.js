e9ba866cbf119fcdecde64e4fbd47df2e0862b35
summer3018 <4121.iskms@gmail.com>
2025-05-19 16:40:59 +0900

    Auto commit

--- js/script.js
+++ js/script.js
@@ -1,1 +1,6 @@
-$(function () {});
+$(function () {
+  $(".js-hamburger, .js-drawer, .js-drawer a").click(function () {
+    $(".js-hamburger").toggleClass("is-active");
+    $(".js-drawer").fadeToggle();
+  });
+});
